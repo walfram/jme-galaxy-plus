794d5ff6020e2216bfcffd11a41a37ed7a99a681
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-11-05 12:34:00 +0200

    Add unit tests for `Effort` class to validate effort calculations based on industry and population values

---
+++ src/test/java/galaxy/domain/planet/EffortTest.java
@@ -0,0 +1,27 @@
+package galaxy.domain.planet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EffortTest {
+
+	@Test
+	void test_effort_industry_500_population_500() {
+		Effort effort = new Effort(new Industry(500.0), new Population(500.0));
+		assertEquals(500.0, effort.value());
+	}
+
+	@Test
+	void test_effort_industry_250_population_500() {
+		Effort effort = new Effort(new Industry(250.0), new Population(500.0));
+		assertEquals(312.5, effort.value());
+	}
+
+	@Test
+	void test_effort_industry_0_population_500() {
+		Effort effort = new Effort(new Industry(0.0), new Population(500.0));
+		assertEquals(125.0, effort.value());
+	}
+
+}
