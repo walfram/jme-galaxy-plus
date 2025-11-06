19c333bb967f629b45a924b908cf1e39465deebb
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-11-06 09:35:03 +0200

    Add `FormattedCoordinates` and `PlanetInfoWidget` to display planet details in the UI

---
+++ src/main/java/galaxy/shared/FormattedCoordinates.java
@@ -0,0 +1,15 @@
+package galaxy.shared;
+
+import galaxy.domain.planet.Coordinates;
+
+public class FormattedCoordinates {
+	private final Coordinates coordinates;
+
+	public FormattedCoordinates(Coordinates coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public String value() {
+		return "%.02f %.02f %.02f".formatted(coordinates.x(), coordinates.y(), coordinates.z());
+	}
+}
