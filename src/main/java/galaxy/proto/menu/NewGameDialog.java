6d7d461801a54d0cc186ebf4ecd9d269725be04d
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-11-11 10:48:21 +0200

    Add dynamic race configuration to `NewGameDialog` using `Spinner` and `Selector`; implement reactive UI updates with `SimpleControl`.

--- src/main/java/galaxy/proto/menu/NewGameDialog.java
+++ src/main/java/galaxy/proto/menu/NewGameDialog.java
@@ -2,11 +2,13 @@ package galaxy.proto.menu;
 
 import com.simsilica.lemur.*;
 import com.simsilica.lemur.component.SpringGridLayout;
+import com.simsilica.lemur.core.VersionedList;
+import com.simsilica.lemur.core.VersionedReference;
 import com.simsilica.lemur.style.ElementId;
-import com.simsilica.lemur.text.DocumentModelFilter;
-import com.simsilica.lemur.text.TextFilters;
-import com.simsilica.lemur.value.TextFieldValueEditor;
+import galaxy.ai.Personality;
+import jme3utilities.SimpleControl;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class NewGameDialog extends Container {
@@ -14,6 +16,8 @@ public class NewGameDialog extends Container {
 	private final Spinner<Integer> raceCount;
 
 	private final Spinner<Integer> planetsPerRace;
+	private final VersionedReference<Integer> raceCountRef;
+	private final Container raceContainer;
 
 	public NewGameDialog(Button accept, Button cancel) {
 		super();
@@ -25,11 +29,22 @@ public class NewGameDialog extends Container {
 
 		form.addChild(new Label("Race count spinner"));
 		raceCount = form.addChild(new Spinner<>(new SequenceModels.ListSequence<>(IntStream.range(2, 128).boxed().toList(), 24)), 1);
+		raceCountRef = raceCount.getModel().createReference();
 
 		form.addChild(new Label("Planets per race"));
-//		planetsPerRace = form.addChild(new TextField("10"), 1);
-		// planetsPerRace = form.addChild(new TextField(new DocumentModelFilter(TextFilters.numeric(), TextFilters.upperCaseTransform())), 1);
 		planetsPerRace = form.addChild(new Spinner<>(new SequenceModels.ListSequence<>(IntStream.range(3, 128).boxed().toList(), 10)), 1);
+
+		raceContainer = addChild(new Container());
+		raceContainer.addControl(new SimpleControl() {
+			@Override
+			protected void controlUpdate(float updateInterval) {
+				if (raceCountRef.update()) {
+					updateRaces();
+				}
+			}
+		});
+
+		updateRaces();
 
 		Container actions = addChild(new Container(new SpringGridLayout(Axis.X, Axis.Y)));
 
@@ -38,6 +53,16 @@ public class NewGameDialog extends Container {
 
 		actions.addChild(accept);
 		actions.addChild(cancel);
+	}
+
+	private void updateRaces() {
+		raceContainer.detachAllChildren();
+
+		for (int i = 0; i < raceCountRef.get(); i++) {
+			raceContainer.addChild(new Label("Race " + (i + 1)));
+			Selector<Personality> selector = raceContainer.addChild(new Selector<>(VersionedList.wrap(List.of(Personality.values()))), 1);
+			selector.setSelectedItem(Personality.NEUTRAL);
+		}
 	}
 
 	public GameConfig config() {
