8ab6fc704263a349cc5746483a903a02665d4066
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2026-09-08 13:28:56 +0300

    feat(json): add `JsonShipTypes` for ship type management via JSON deserialization

---
+++ galaxy-alternative/src/main/java/galaxy/json/JsonShipTypes.java
@@ -0,0 +1,25 @@
+package galaxy.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import galaxy.ShipType;
+import galaxy.ShipTypes;
+
+public final class JsonShipTypes implements ShipTypes {
+
+	private final JsonNode src;
+
+	public JsonShipTypes(JsonNode src) {
+		this.src = src;
+	}
+
+	@Override
+	public int size() {
+		return src.size();
+	}
+
+	@Override
+	public ShipType typeById(String id) {
+		return new JsonShipType(src.get(id));
+	}
+
+}
