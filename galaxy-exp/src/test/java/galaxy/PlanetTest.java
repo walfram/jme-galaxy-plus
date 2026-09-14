237e66c4fe5d109f0dae5578833154b61a28d7ab
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2026-09-14 15:12:32 +0300

    refactor(core): remove obsolete tests and streamline `ClassicGeneratedPlanets` imports
    - Deleted `PlanetTest` and `PopulationTest` as they were unused and outdated.
    - Consolidated imports in `ClassicGeneratedPlanets` by utilizing wildcard for `galaxy.planet`.
    - Replaced redundant `Planet.Ind` and `Planet.Pop` constructors with `Industry` and `Population` to align with updated abstractions.

--- galaxy-exp/src/test/java/galaxy/PlanetTest.java
+++
@@ -1,4 +0,0 @@
-package galaxy;
-
-public class PlanetTest {
-}
--- map-generator/src/main/java/generator/classic/ClassicGeneratedPlanets.java
+++ map-generator/src/main/java/generator/classic/ClassicGeneratedPlanets.java
@@ -5,10 +5,7 @@ import distribution.PlanetType;
 import distribution.WeightedDistribution;
 import distribution.classic.ClassicPlanetDistribution;
 import galaxy.Planet;
-import galaxy.planet.Coordinates;
-import galaxy.planet.PlanetId;
-import galaxy.planet.Resources;
-import galaxy.planet.Size;
+import galaxy.planet.*;
 import generator.GeneratedPlanets;
 import hex.grid.Vector2d;
 import jme3utilities.math.noise.Generator;
@@ -58,13 +55,13 @@ public class ClassicGeneratedPlanets implements GeneratedPlanets {
 
 		List<Vector2d> origins = new HexGridOrigins(raceCount).asList(generator);
 		origins.stream()
-				.map(v -> new Planet(new Coordinates(v.x(), v.y()), new Size(1000.0), new Resources(10.0), new Planet.Ind(1000.0), new Planet.Pop(1000.0)))
+				.map(v -> new Planet(new Coordinates(v.x(), v.y()), new Size(1000.0), new Resources(10.0), new Industry(1000.0), new Population(1000.0)))
 				.map(p -> {
 					Vector2d p1Offset = dwOffset.apply(new Vector2d(p.coordinates().x(), p.coordinates().y()));
-					Planet p1 = new Planet(new Coordinates(p1Offset.x(), p1Offset.y()), new Size(500.0), new Resources(10.0), new Planet.Ind(500.0), new Planet.Pop(500.0));
+					Planet p1 = new Planet(new Coordinates(p1Offset.x(), p1Offset.y()), new Size(500.0), new Resources(10.0), new Industry(500.0), new Population(500.0));
 
 					Vector2d p2Offset = dwOffset.apply(new Vector2d(p.coordinates().x(), p.coordinates().y()));
-					Planet p2 = new Planet(new Coordinates(p2Offset.x(), p2Offset.y()), new Size(500.0), new Resources(10.0), new Planet.Ind(500.0), new Planet.Pop(500.0));
+					Planet p2 = new Planet(new Coordinates(p2Offset.x(), p2Offset.y()), new Size(500.0), new Resources(10.0), new Industry(500.0), new Population(500.0));
 
 					return List.of(p, p1, p2);
 				})
